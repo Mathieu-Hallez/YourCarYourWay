12590ff65c1b0bd9ec238355d32bfd5efc5b30c3
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-12-23 22:57:24 +0100

    Add Models used by user

---
+++ back/src/main/java/me/yourcaryourway/YourCarYourWay/models/Address.java
@@ -0,0 +1,40 @@
+package me.yourcaryourway.YourCarYourWay.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.apache.commons.lang3.builder.ToStringExclude;
+
+@Entity
+@Table(name = "Address")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @ToStringExclude
+    private Long id;
+
+    @NotNull
+    @Column(name = "street_number")
+    private Integer streetNumber;
+
+    @NotNull
+    @Column(name = "street_name")
+    private String streetName;
+
+    @NotNull
+    private Integer postcode;
+
+    @NotNull
+    @Column(name = "locality_name")
+    private String localityName;
+
+    @NotNull
+    private String country;
+}
---
+++ back/src/main/java/me/yourcaryourway/YourCarYourWay/models/User.java
@@ -0,0 +1,56 @@
+package me.yourcaryourway.YourCarYourWay.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.apache.commons.lang3.builder.ToStringExclude;
+
+import java.sql.Date;
+
+@Entity
+@Data
+@Table(name = "User")
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ToStringExclude
+    private Long id;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String lastname;
+
+    @NotNull
+    private String firstname;
+
+    @NotNull
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "role", referencedColumnName = "id", nullable = false)
+    private Role role;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "credit_card_id", referencedColumnName = "id")
+    private CreditCard creditCard;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "address_id", nullable = false)
+    private Address address;
+}
