59bfbfa04eb6d8cb0eecfe980d103a9c426c544e
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-12-28 20:18:40 +0100

    Disable security

--- back/src/main/java/me/yourcaryourway/YourCarYourWay/configurations/SpringSecurityConfig.java
+++ back/src/main/java/me/yourcaryourway/YourCarYourWay/configurations/SpringSecurityConfig.java
@@ -53,8 +53,9 @@ public class SpringSecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .authorizeHttpRequests(
                         auth -> {
-                            auth.requestMatchers(PUBLIC_PATHS).permitAll();
-                            auth.anyRequest().authenticated();
+//                            auth.requestMatchers(PUBLIC_PATHS).permitAll();
+//                            auth.anyRequest().authenticated();
+                            auth.anyRequest().permitAll();
                         }
                 )
                 .httpBasic(Customizer.withDefaults())
