95a046c5b71a16f61d2f2e1db3a619259cbb82c4
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-12-29 20:09:37 +0100

    Create message text input component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  standalone: false
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'front';
--- front/src/app/components/message-text-input/message-text-input.component.ts
+++ front/src/app/components/message-text-input/message-text-input.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-message-text-input',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './message-text-input.component.html',
   styleUrl: './message-text-input.component.scss'
 })
 export class MessageTextInputComponent {
+  private formBuilder = inject(FormBuilder)
+  
+  messageForm : FormGroup = this.formBuilder.group({
+    content: [
+      '',
+      [
+        Validators.required
+      ]
+    ]
+  });
+
+  onSendMessage(): void {
+    // TODO Send message to Websocket
+  }
 
 }
