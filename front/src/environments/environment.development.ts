b8a23ab041e968a9b0e705394c8af35f15637c5b
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-12-30 21:59:21 +0100

    Generate  environement variables

---
+++ front/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseURI: "http://localhost:3001",
+    chatURI: "http://localhost:3002"
+};
---
+++ front/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseURI: "http://localhost:3001",
+    chatURI: "http://localhost:3002"
+};
