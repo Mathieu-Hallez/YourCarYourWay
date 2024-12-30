c2fde22e8ee29bc56f639214cb722039ad21dec1
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-12-30 23:41:46 +0100

    Implement contact tile and list

--- front/src/app/components/contact-list/contact-list.component.ts
+++ front/src/app/components/contact-list/contact-list.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Contact } from '../../models/Contact';
+import { ContactTileComponent } from "../contact-tile/contact-tile.component";
+import { Subject, takeUntil } from 'rxjs';
+import { ChatService } from '../../services/api/chat/chat.service';
+import { ContactDto } from '../../interfaces/ContactDto';
 
 @Component({
   selector: 'app-contact-list',
-  imports: [],
+  imports: [ContactTileComponent],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss'
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit, OnDestroy {
 
+    private chatService = inject(ChatService);
+
+    private destroy$ : Subject<boolean> = new Subject<boolean>();
+
+    contacts : Array<Contact> = [];
+
+    ngOnInit(): void {
+        this.chatService.getContacts().pipe(
+            takeUntil(this.destroy$)
+        ).subscribe({
+            next: (contactsDto) => this.fetchContacts(contactsDto)
+        })
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next(true);
+    }
+
+    private fetchContacts(contactsDto : Array<ContactDto>) {
+        this.contacts = contactsDto.map((contactDto) => {
+            return new Contact(contactDto.firstname, contactDto.lastname, contactDto.email)
+        });
+    }
 }
--- front/src/app/components/contact-tile/contact-tile.component.ts
+++ front/src/app/components/contact-tile/contact-tile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Contact } from '../../models/Contact';
 
 @Component({
   selector: 'app-contact-tile',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './contact-tile.component.scss'
 })
 export class ContactTileComponent {
-
+  @Input()
+  contact! : Contact;
 }
