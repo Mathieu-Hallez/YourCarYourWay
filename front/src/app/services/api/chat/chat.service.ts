be52fca2504dfb2c265da1a9c355c8895e2e4d7a
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-12-30 23:41:24 +0100

    Add get contact endpoint

--- front/src/app/services/api/chat/chat.service.ts
+++ front/src/app/services/api/chat/chat.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ConversationDto } from '../../../interfaces/ConversationDto';
+import { ContactDto } from '../../../interfaces/ContactDto';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,10 @@ export class ChatService {
 
   getConversation(senderEmail : string, receiverEmail : string) : Observable<ConversationDto> {
     return this.httpClient.get<ConversationDto>(`${this.pathService}/conversation?sender=${senderEmail}&receiver=${receiverEmail}`);
+  }
+
+  getContacts() : Observable<Array<ContactDto>> {
+    return this.httpClient.get<Array<ContactDto>>(`${this.pathService}/contact`);
   }
 
 }
