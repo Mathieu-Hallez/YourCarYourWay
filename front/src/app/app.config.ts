03edff2a1170753fa5d87905867c0d2835adac49
Mathieu-Hallez <mathew.hallez@gmail.com>
2024-12-30 21:59:53 +0100

    Add HttpClient and interceptor

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { baseUriInterceptor } from './interceptors/base-uri.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync('noop'),
+    provideHttpClient(
+      withInterceptors([baseUriInterceptor])
+    )
+  ]
 };
