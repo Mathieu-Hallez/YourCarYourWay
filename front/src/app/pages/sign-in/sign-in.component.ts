006662a2b390ad6777d6999b21a7ad105a1f9b1e
Mathieu-Hallez <mathew.hallez@gmail.com>
2025-01-01 15:21:23 +0100

    Create sign-in page and form

--- front/src/app/pages/sign-in/sign-in.component.ts
+++ front/src/app/pages/sign-in/sign-in.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
 
+    formObject : any = {
+        email: '',
+        password: ''
+    };
+
+    onSignIn(): void {
+        const formValue = this.formObject;
+        console.log("Submit: " + JSON.stringify(formValue));
+    }
 }
