a7592148982933a00c2e5315895c30e086f08742
Mathieu-Hallez <mathew.hallez@gmail.com>
2025-01-02 01:13:55 +0100

    Implement signIn validation form

--- front/src/app/pages/sign-in/sign-in.component.ts
+++ front/src/app/pages/sign-in/sign-in.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { SessionService } from '../../services/session/session.service';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
@@ -11,13 +14,32 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class SignInComponent {
 
+    private sessionService = inject(SessionService);
+    errorMessage : string | null = null;
+
     formObject : any = {
         email: '',
         password: ''
     };
 
+    constructor(
+        private router : Router
+    ) {}
+
     onSignIn(): void {
-        const formValue = this.formObject;
-        console.log("Submit: " + JSON.stringify(formValue));
+        this.sessionService.signIn(this.formObject.email, this.formObject.password)
+            .pipe(
+                take(1)
+            )
+            .subscribe({
+                next: (value) => {
+                    if(value) {
+                        this.router.navigateByUrl('/chat');
+                    }
+                },
+                error: (err) => {
+                    this.errorMessage = err.message ?? 'An error occured!'
+                }
+            });
     }
 }
