5b57c799d474084a7d12ea456645200b061999be
Mathieu-Hallez <mathew.hallez@gmail.com>
2025-01-02 01:08:21 +0100

    Create authentication service

---
+++ front/src/app/services/api/authentication/authentication.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenDto } from '../../../interfaces/TokenDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private pathService : string = '/api/authentication';
+
+  constructor(
+    private httpClient : HttpClient
+  ) { }
+
+  login(email : string, password : string) : Observable<TokenDto> {
+    return this.httpClient.post<TokenDto>(`${this.pathService}/login`, {email: email, password: password});
+  }
+}
